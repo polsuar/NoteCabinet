78de4aecd99e5fe68099c0ecde38812e8b8e8255
Pol Suar <pablors88@gmail.com>
2021-04-12 13:18:48 -0300

    Add styling to welcome and application.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     root "docs#index", as: "authenticated_root"
   end
   root 'welcome#index'
-  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
